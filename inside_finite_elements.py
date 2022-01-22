07380c620a8e94a07f8e6d7f8ec1da8b1962f72a
Benjamin Menkuec <benjamin@menkuec.de>
2022-01-23 00:06:31 +0100

    added rectangularCriss function

--- inside_finite_elements.py
+++ inside_finite_elements.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly
+import plotly.graph_objects as go
+
 
 def numberOfVertices(G):
     return G['xp'].shape[0]
@@ -33,6 +34,59 @@ def shapeFunctionGradients():
 def shapeFunctionValues(xi):
     return [1, 0, 0] + shapeFunctionGradients() @ xi
 
+def plotShapeFunctions():
+    x = np.linspace(0,1,100)
+    y = np.linspace(0,1,100)
+    grid = np.meshgrid(x,y)
+    coords = np.array([grid[0].flatten(), grid[1].flatten()]).T
+    coordsMask = [coords[i][0] + coords[i][1] <= 1 for i in range(0,coords.shape[0])]
+    triangleCoords = coords[coordsMask]
+    val = np.zeros([triangleCoords.shape[0],3])
+    for i in range (triangleCoords.shape[0]):
+        val[i] = shapeFunctionValues([triangleCoords[i][0], triangleCoords[i][1]])
+
+    if False:
+        fig = plt.figure(figsize =(14, 9))
+        ax = fig.add_subplot(1, 3, 1, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,0])
+        ax.azim = -90
+        ax = fig.add_subplot(1, 3, 2, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,1])
+        ax = fig.add_subplot(1, 3, 3, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,2])
+        plt.show()
+    else:
+        # from plotly.subplots import make_subplots
+        # fig = make_subplots(rows=1, cols=1)
+        fig = go.Figure()
+
+        data = np.ones(triangleCoords.shape[0]) - triangleCoords[:,0] - triangleCoords[:,1]
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=data, color='green',opacity=0.90))
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,1], color='blue',opacity=0.90))
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,2], color='red',opacity=0.90))
+        #fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=np.zeros(triangleCoords.shape[0]), color='gray'))
+        fig.update_layout(
+            scene = dict(
+                xaxis = dict(nticks=4, range=[0,1]),
+                            yaxis = dict(nticks=4, range=[0,1]),
+                            zaxis = dict(nticks=4, range=[0,1]),),
+            width=1000,
+            height=1000,
+            margin=dict(r=10, l=10, b=10, t=10))        
+        fig.show()
+
+def rectangularCriss(w, h):
+    G = dict()
+    G['xp'] = np.zeros([w*h,2])
+    G['pt'] = []
+    for y in range (0,h):
+        for x in range (0,w):
+            G['xp'][x+w*y] = [x,y]
+            if x < (w-1) and y < (h-1):
+                G['pt'].append([x+w*y, (x+1)+w*y, x+w*(y+1)])
+                G['pt'].append([(x+1)+w*y, (x+1)+w*(y+1), x+w*(y+1)])
+    return G
+
 def main():
     G = {}
     # store point coordinates 'xp'
@@ -53,23 +107,11 @@ def main():
     xi = localCoordinate(G, 0, p)
     print(f'point ({p[0]:d}, {p[1]:d}) of global triangle transformed to ref triangle = ({xi[0]:f}, {xi[1]:f})')
     
-    fig = plt.figure(figsize =(14, 9))
-    x = np.linspace(0,1,100)
-    y = np.linspace(0,1,100)
-    grid = np.meshgrid(x,y)
-    coords = np.array([grid[0].flatten(), grid[1].flatten()]).T
-    coordsMask = [coords[i][0] <= coords[i][1] for i in range(0,coords.shape[0])]
-    triangleCoords = coords[coordsMask]
-    val = np.zeros([triangleCoords.shape[0],3])
-    for i in range (triangleCoords.shape[0]):
-        val[i] = shapeFunctionValues([triangleCoords[i][0], triangleCoords[i][1]])
-    ax = fig.add_subplot(1, 3, 1, projection='3d')
-    ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,0])
-    ax.azim = -90
-    ax = fig.add_subplot(1, 3, 2, projection='3d')
-    ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,1])
-    ax = fig.add_subplot(1, 3, 3, projection='3d')
-    ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,2])
+    #plotShapeFunctions()
+    G = rectangularCriss(10,5)
+    plt.scatter(G['xp'][:,0],G['xp'][:,1])
+    for edge in G['pt']:
+        plt.plot([G['xp'][edge[0],0], G['xp'][edge[1],0], G['xp'][edge[2],0], G['xp'][edge[0],0]],[G['xp'][edge[0],1], G['xp'][edge[1],1], G['xp'][edge[2],1], G['xp'][edge[0],1]])
     plt.show()
 
 if __name__ == "__main__":
