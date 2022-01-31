grad() works, maybe still factor 2 wrong

formulation.py
@@ -9,6 +9,7 @@ import pkg_resources
 from scipy.sparse import *
 from parameter import *
 from region import region
+from field import *
 
 if 'petsc4py' in pkg_resources.working_set.by_key:
     hasPetsc = True
@@ -32,55 +33,6 @@ def localCoordinate(G, t, x):
     B, x1 = transformationJacobian(G, t)
     xi,_,_,_ = np.linalg.lstsq(B, x-x1, rcond=None)
     return xi
-
-def shapeFunctionGradients():
-    return np.array([[-1, -1],
-        [1, 0],
-        [0, 1]])
-
-def shapeFunctionValues(xi):
-    return [1, 0, 0] + shapeFunctionGradients() @ xi
-
-def plotShapeFunctions():
-    x = np.linspace(0,1,100)
-    y = np.linspace(0,1,100)
-    grid = np.meshgrid(x,y)
-    coords = np.array([grid[0].flatten(), grid[1].flatten()]).T
-    coordsMask = [coords[i][0] + coords[i][1] <= 1 for i in range(0,coords.shape[0])]
-    triangleCoords = coords[coordsMask]
-    val = np.zeros([triangleCoords.shape[0],3])
-    for i in range (triangleCoords.shape[0]):
-        val[i] = shapeFunctionValues([triangleCoords[i][0], triangleCoords[i][1]])
-
-    if False:
-        fig = plt.figure(figsize =(14, 9))
-        ax = fig.add_subplot(1, 3, 1, projection='3d')
-        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,0])
-        ax.azim = -90
-        ax = fig.add_subplot(1, 3, 2, projection='3d')
-        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,1])
-        ax = fig.add_subplot(1, 3, 3, projection='3d')
-        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,2])
-        plt.show()
-    else:
-        # from plotly.subplots import make_subplots
-        # fig = make_subplots(rows=1, cols=1)
-        fig = go.Figure()
-
-        data = np.ones(triangleCoords.shape[0]) - triangleCoords[:,0] - triangleCoords[:,1]
-        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=data, color='green',opacity=0.90))
-        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,1], color='blue',opacity=0.90))
-        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,2], color='red',opacity=0.90))
-        #fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=np.zeros(triangleCoords.shape[0]), color='gray'))
-        fig.update_layout(
-            scene = dict(
-                xaxis = dict(nticks=4, range=[0,1]),
-                            yaxis = dict(nticks=4, range=[0,1]),
-                            zaxis = dict(nticks=4, range=[0,1]),),
-            width=1000,
-            height=1000,
-            margin=dict(r=10, l=10, b=10, t=10))        
-        fig.show()
 
 # integral grad(u) * sigma * grad(tf(u)) 
 def stiffnessMatrix(sigmas, region=[]):

parameter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from mesh import *
+from field import *
 import region
 
 parameterList = []
@@ -85,24 +86,15 @@ class parameter:
             vertexValues[mesh()['pt'][n][2]] = triangleValues[n]
         return vertexValues
 
-# TODO: implement this routine without using pyvista/vtk
 def grad(u):
     m = numberOfTriangles()
     grads = np.zeros((m,3))
+    sfGrads = shapeFunctionGradients()
     for triangleIndex, triangle in enumerate(mesh()['pt']):    
-        p1 = np.append(mesh()['xp'][triangle[0]], u[triangle[0]])
-        p2 = np.append(mesh()['xp'][triangle[1]], u[triangle[1]])
-        p3 = np.append(mesh()['xp'][triangle[2]], u[triangle[2]])
-        mat = np.column_stack([p1.T, p2.T, p3.T])
-        matA = np.copy(mat)
-        matA[:,0] = np.ones((1,3))
-        matB = np.copy(mat)
-        matB[:,1] = np.ones((1,3))
-        matC = np.copy(mat)
-        matC[:,2] = np.ones((1,3))
-        det = np.linalg.det(mat)
-        grads[triangleIndex] = [np.linalg.det(matA)/det, np.linalg.det(matB)/det, np.linalg.det(matC)/det]
-    return 
+        jac,_ = transformationJacobian(triangleIndex)        
+        invJac = np.linalg.inv(jac)
+        grads[triangleIndex] = np.append(invJac.T @ sfGrads.T @ u[triangle], 0)
+    return grads
     # points = np.hstack([mesh()['xp'], np.zeros((n,1))]) # add z coordinate
     # cells = (np.hstack([(3*np.ones((m,1))), mesh()['pt']])).ravel().astype(np.int64)
     # celltypes = np.empty(m, np.uint8)