3b0c1b4e5549540fd2b8dbdb57352e6bfc439f21
Benjamin Menkuec <benjamin@menkuec.de>
2022-02-01 14:54:17 +0100

    add missing file

---
+++ field.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mesh import *
+
+def shapeFunctionGradients():
+    if mesh()['problemDimension'] == 2:
+        return np.array([[-1, -1],
+                        [1, 0],
+                        [0, 1]])
+    else:
+        return np.array([[-1, -1, -1],  # TODO verify that this is correct
+                        [1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1]])
+
+def shapeFunctionValues(xi):
+    return [1, 0, 0] + shapeFunctionGradients() @ xi
+
+def plotShapeFunctions():
+    x = np.linspace(0,1,100)
+    y = np.linspace(0,1,100)
+    grid = np.meshgrid(x,y)
+    coords = np.array([grid[0].flatten(), grid[1].flatten()]).T
+    coordsMask = [coords[i][0] + coords[i][1] <= 1 for i in range(0,coords.shape[0])]
+    triangleCoords = coords[coordsMask]
+    val = np.zeros([triangleCoords.shape[0],3])
+    for i in range (triangleCoords.shape[0]):
+        val[i] = shapeFunctionValues([triangleCoords[i][0], triangleCoords[i][1]])
+
+    if False:
+        fig = plt.figure(figsize =(14, 9))
+        ax = fig.add_subplot(1, 3, 1, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,0])
+        ax.azim = -90
+        ax = fig.add_subplot(1, 3, 2, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,1])
+        ax = fig.add_subplot(1, 3, 3, projection='3d')
+        ax.plot_trisurf(triangleCoords[:,0],triangleCoords[:,1],val[:,2])
+        plt.show()
+    else:
+        # from plotly.subplots import make_subplots
+        # fig = make_subplots(rows=1, cols=1)
+        fig = go.Figure()
+
+        data = np.ones(triangleCoords.shape[0]) - triangleCoords[:,0] - triangleCoords[:,1]
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=data, color='green',opacity=0.90))
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,1], color='blue',opacity=0.90))
+        fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=val[:,2], color='red',opacity=0.90))
+        #fig.add_trace(go.Mesh3d(x=triangleCoords[:,0], y=triangleCoords[:,1], z=np.zeros(triangleCoords.shape[0]), color='gray'))
+        fig.update_layout(
+            scene = dict(
+                xaxis = dict(nticks=4, range=[0,1]),
+                            yaxis = dict(nticks=4, range=[0,1]),
+                            zaxis = dict(nticks=4, range=[0,1]),),
+            width=1000,
+            height=1000,
+            margin=dict(r=10, l=10, b=10, t=10))        
+        fig.show()    
