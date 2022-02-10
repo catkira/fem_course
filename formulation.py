fix edge element calculation

formulation.py
@@ -19,6 +19,7 @@ if 'petsc4py' in pkg_resources.working_set.by_key:
     from petsc4py import PETSc
 else:
     print("Warning: no petsc4py found, solving will be very slow!")
+np.set_printoptions(linewidth=400)    
 
 from mesh import *
 
@@ -430,7 +431,7 @@ def solve(A, b, method='np'):
             ksp.setOperators(Ap)        
             ksp.setFromOptions()
             #ksp.setType('cg')  # conjugate gradient
-            ksp.getPC().setType('lu')
+            #ksp.getPC().setType('lu')
             #ksp.getPC().setType('cholesky') # cholesky
             #ksp.getPC().setType('icc') # incomplete cholesky
             print(f'Solving with: {ksp.getType():s}')
@@ -696,13 +697,8 @@ def exampleHMagnetCurl():
     print(f"{bcolors.OKGREEN}assembled in {stop - start:.2f} s{bcolors.ENDC}")       
     u = solve(A, b, 'petsc')    
     storeInVTK(u, "h_magnetCurl_u.vtk", writePointData=True)    
-    h = -field.grad(u, dim=3)
-    storeInVTK(h, "h_magnetCurl_h.vtk")
-    mus = mu.getValues()  
-    m = numberOfTetraeders()       
-    brs = br.getValues()
-    b = np.column_stack([mus,mus,mus])*h + brs  # this is a bit ugly
-    storeInVTK(b,"h_magnetCurl_b.vtk")    
+    b = field.curl(u, dim=3)
+    storeInVTK(b, "h_magnetCurl_b.vtk")
     print(f'b_max = {max(np.linalg.norm(b,axis=1)):.4f}')    
     assert(abs(max(np.linalg.norm(b,axis=1)) - 3.2898) < 1e-3)
 

mesh.py
@@ -65,10 +65,10 @@ def computeSigns():
     global mesh
     if mesh['problemDimension'] == 2:
         tmp = mesh['pt'][:,[1,2,0]] - mesh['pt'][:,[2,0,1]]
-        mesh['signs2d'] = np.multiply(tmp, 1/abs(tmp)).astype(np.int8)
+        mesh['signs2d'] = np.multiply(tmp, 1/abs(tmp)).round()
     elif mesh['problemDimension'] == 3:
         tmp = mesh['ptt'][:,[0,0,0,1,2,3]] - mesh['ptt'][:,[1, 2, 3, 2, 3, 1]]
-        mesh['signs3d'] = np.multiply(tmp, 1/abs(tmp)).astype(np.int8)
+        mesh['signs3d'] = np.multiply(tmp, 1/abs(tmp)).round()
 
 def edgeSigns():
     if mesh['problemDimension'] == 2:
@@ -115,14 +115,14 @@ def computeEdges3d():
                         mesh['ett'][tetraederIndex][edgeIndex] = storedIndex - 1
      # compute triangle-to-edges list
         if True:
-            vertices2d = np.zeros((mesh['pt'].shape[0]*6,2))
-            vertices2d[0::6] = mesh['pt'][:,[1,2]]            
-            vertices2d[1::6] = mesh['pt'][:,[2,0]]            
-            vertices2d[2::6] = mesh['pt'][:,[0,1]]            
+            vertices2d = np.zeros((mesh['pt'].shape[0]*3,2))
+            vertices2d[0::3] = mesh['pt'][:,[1,2]]            
+            vertices2d[1::3] = mesh['pt'][:,[2,0]]            
+            vertices2d[2::3] = mesh['pt'][:,[0,1]]            
             vertices2d.sort(axis=1)
             verticesMixed = np.row_stack((vertices3d, vertices2d))
             _,J,I = np.unique(verticesMixed, return_index=True, return_inverse=True, axis=0)
-            mesh['et'] = I[mesh['ett'].shape[0]*6:].reshape(mesh['pt'].shape[0],6)[:,0:3]
+            mesh['et'] = I[mesh['ett'].shape[0]*6:].reshape(mesh['pt'].shape[0],3)
             mesh['pe'] = verticesMixed[J,:]  # there should not be much new edges from surface elements, but it can happen
         if False:      
             for triangleIndex, triangle in enumerate(mesh['pt']):