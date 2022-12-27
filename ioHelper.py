3120745aee94f67810c1a270b1e1587e9624248e
Benjamin Menküc <benjamin@menkuec.de>
2022-12-27 17:04:55 +0100

    cleanup

--- ioHelper.py
+++ ioHelper.py
@@ -1,20 +1,22 @@
+import sys
 import numpy as np
 import time
 import vtk
+import pyvista as pv
 
 import field as fd
 from parameter import Parameter
-from mesh import *
-from region import Region
+import mesh as m
 
 # double values have around 16 decimals
 def f2s(inputValue):
-    return ('%.15f' % inputValue).rstrip('0').rstrip('.')
+    return f'{inputValue:.15f}'.rstrip("0").rstrip(".")
+
 
 # u can be of type parameter or a list
 # if its a parameter, it has to be defined for all regions in its dimension
 # parameter can only be plotted if its on the highest dimension of the mesh
-def storeInVTK(u, filename, writePointData: bool = False, field = []):
+def storeInVTK(u, filename, writePointData: bool = False, field=[]):
     if isinstance(u, Parameter):
         if field == []:
             field = fd.globalField  # TODO: this only works if only one field is defined !
@@ -25,111 +27,116 @@ def storeInVTK(u, filename, writePointData: bool = False, field = []):
     elif isinstance(u, fd.Field):
         field = u
         u = u.elementValues.values
-    else: # assume u is just an array with values
+    else:  # assume u is just an array with values
         if field == []:
             field = fd.globalField  # TODO: this only works if only one field is defined !
     storeInVTK2(u, filename, writePointData, field)
 
+
 def storeInVTK2(u, filename, writePointData, field):
-    start = time.time()    
-    if getMesh()['problemDimension'] == 2:
-        ppe = 3 # points per element
-        #elementContainer = getMesh()['pt']
+    start = time.time()
+    if m.getMesh()["problemDimension"] == 2:
+        ppe = 3  # points per element
+        # elementContainer = getMesh()['pt']
         elementContainer = field.getElements(dim=2, nodesOnly=True, translate=False)
         cellType = vtk.VTK_LAGRANGE_TRIANGLE
     else:
-        #elementContainer = getMesh()['ptt']
+        # elementContainer = getMesh()['ptt']
         elementContainer = field.getElements(dim=3, nodesOnly=True, translate=False)
-        ppe = 4 # points per element
+        ppe = 4  # points per element
         cellType = vtk.VTK_LAGRANGE_TETRAHEDRON
-    m = len(elementContainer)
+    numElements = len(elementContainer)
 
     if writePointData:
         if field.isEdgeField():
-            if getMesh()['problemDimension'] == 3:
+            if m.getMesh()["problemDimension"] == 3:
                 ppe = 6
             # TODO: calculate barycenter of each edge
             # but its not so easy, because the barycenter point is not in mesh['xp']
             # so for now, just take one point of the edge
-            elementContainer = getMesh()['ett'] 
-            elementContainer = ((getMesh()['pe'][elementContainer.ravel()])[:,0]).reshape((len(elementContainer),ppe))
+            elementContainer = m.getMesh()["ett"]
+            elementContainer = ((m.getMesh()["pe"][elementContainer.ravel()])[:, 0]).reshape((len(elementContainer),
+                ppe))
             assert len(u) >= np.max(elementContainer) + 1, "u has to be defined for all elements in the mesh"
         else:
-            #assert len(u) == np.max(elementContainer) + 1, "u has to be defined for all elements in the mesh"
+            # assert len(u) == np.max(elementContainer) + 1, "u has to be defined for all elements in the mesh"
             # TODO: figure out while this assert is not working with inductionheating
             pass
     else:
-        assert len(u) == m, "u has to be defined for all elements in the field"
+        assert len(u) == numElements, "u has to be defined for all elements in the field"
 
-    scalarValue = (not isinstance(u[0], list)) and (not type(u[0]) is np.ndarray)
-    with open(filename, 'w') as file:
+    scalarValue = not isinstance(u[0], (list, np.ndarray))
+    with open(filename, "w", encoding="utf-8") as file:
         vtktxt = str()
         vtktxt += "# vtk DataFile Version 4.2\nu\nASCII\nDATASET UNSTRUCTURED_GRID\n\n"
-        vtktxt += f'POINTS {m*ppe:d} double\n'
+        vtktxt += f"POINTS {numElements*ppe:d} double\n"
         for element in elementContainer:
             for point in element:
-                coords = getMesh()['xp'][point]
+                coords = m.getMesh()["xp"][point]
                 if len(coords) == 2:
                     vtktxt += f2s(coords[0]) + " " + f2s(coords[1]) + " 0 "
                 else:
                     vtktxt += f2s(coords[0]) + " " + f2s(coords[1]) + " " + f2s(coords[2]) + " "
-            vtktxt += '\n'             
-        vtktxt += f'\nCELLS {m:d} {m*(ppe+1):d}\n'
+            vtktxt += "\n"
+        vtktxt += f"\nCELLS {numElements:d} {numElements*(ppe+1):d}\n"
         if ppe == 3:
             for elementIndex, element in enumerate(elementContainer):
-                vtktxt += f'3 {elementIndex*ppe:d} {elementIndex*ppe+1:d} {elementIndex*ppe+2:d}\n'
+                vtktxt += f"3 {elementIndex*ppe:d} {elementIndex*ppe+1:d} {elementIndex*ppe+2:d}\n"
         else:
             for elementIndex, element in enumerate(elementContainer):
-                vtktxt += f'4 {elementIndex*ppe:d} {elementIndex*ppe+1:d} {elementIndex*ppe+2:d} {elementIndex*ppe+3:d}\n'
-        vtktxt += f'\nCELL_TYPES {m:d}\n'
+                vtktxt += (
+                    f"4 {elementIndex*ppe:d} {elementIndex*ppe+1:d} {elementIndex*ppe+2:d} {elementIndex*ppe+3:d}\n"
+                )
+        vtktxt += f"\nCELL_TYPES {numElements:d}\n"
         for element in elementContainer:
             vtktxt += f"{cellType:d}\n"
         if writePointData:
             # TODO implement write edge data
             if scalarValue:
-                vtktxt += f'\nPOINT_DATA {m*ppe:d}\nSCALARS u double\nLOOKUP_TABLE default\n'
+                vtktxt += f"\nPOINT_DATA {numElements*ppe:d}\nSCALARS u double\nLOOKUP_TABLE default\n"
             else:
-                vtktxt += f'\nPOINT_DATA {m*ppe:d}\nVECTORS u double\n'
+                vtktxt += f"\nPOINT_DATA {numElements*ppe:d}\nVECTORS u double\n"
             for element in elementContainer:
                 for point in element:
                     if scalarValue:
                         vtktxt += f2s(u[point]) + "\n"
                     else:
-                        if getMesh()['problemDimension'] == 2:
+                        if m.getMesh()["problemDimension"] == 2:
                             vtktxt += f2s(u[point][0]) + " " + f2s(u[point][1]) + " 0"
                         else:
                             vtktxt += f2s(u[point][0]) + " " + f2s(u[point][1]) + " " + f2s(u[point][2])
                         vtktxt += "\n"
         else:
             if scalarValue:
-                vtktxt += f'\nCELL_DATA {m:d}\nSCALARS u double\nLOOKUP_TABLE default\n'
+                vtktxt += f"\nCELL_DATA {numElements:d}\nSCALARS u double\nLOOKUP_TABLE default\n"
             else:
-                vtktxt += f'\nCELL_DATA {m:d}\nVECTORS u double\n'
+                vtktxt += f"\nCELL_DATA {numElements:d}\nVECTORS u double\n"
             for elementIndex, element in enumerate(elementContainer):
                 if scalarValue:
                     vtktxt += f2s(u[elementIndex]) + "\n"
                 else:
-                    if getMesh()['problemDimension'] == 2:
+                    if m.getMesh()["problemDimension"] == 2:
                         vtktxt += f2s(u[elementIndex][0]) + " " + f2s(u[elementIndex][1]) + " 0"
                     else:
-                        vtktxt += f2s(u[elementIndex][0]) + " " + f2s(u[elementIndex][1]) + " " + f2s(u[elementIndex][2])
+                        vtktxt += (
+                            f2s(u[elementIndex][0]) + " " + f2s(u[elementIndex][1]) + " " + f2s(u[elementIndex][2])
+                        )
                     vtktxt += "\n"
         file.write(vtktxt)
-    stop = time.time()                    
-    print(f'written {filename:s} in {stop-start:.2f}s')
-                
+    stop = time.time()
+    print(f"written {filename:s} in {stop-start:.2f}s")
+
+
 # using pyvista save writes vtk files in version 5, which generates wrong gradients
 # when using the gradient filter in paraview
-def storeInVTKpv(u, filename, writePointData = False):
-    import pyvista as pv
-    import vtk    
-    n = numberOfVertices()    
-    m = numberOfTriangles()    
-    points = np.hstack([mesh()['xp'], np.zeros((n,1))]) # add z coordinate
-    cells = (np.hstack([(3*np.ones((m,1))), mesh()['pt']])).ravel().astype(np.int64)
-    celltypes = np.empty(m, np.uint8)
-    #celltypes[:] = vtk.VTK_TRIANGLE    
-    celltypes[:] = vtk.VTK_LAGRANGE_TRIANGLE    
+def storeInVTKpv(u, filename, writePointData=False):
+    n = m.numberOfVertices()
+    numTriangles = m.numberOfTriangles()
+    points = np.hstack([m.mesh()["xp"], np.zeros((n, 1))])  # add z coordinate
+    cells = (np.hstack([(3 * np.ones((numTriangles, 1))), m.mesh()["pt"]])).ravel().astype(np.int64)
+    celltypes = np.empty(numTriangles, np.uint8)
+    # celltypes[:] = vtk.VTK_TRIANGLE
+    celltypes[:] = vtk.VTK_LAGRANGE_TRIANGLE
     grid = pv.UnstructuredGrid(cells, celltypes, points)
     if not isinstance(u, Parameter):
         grid.point_data["u"] = u
@@ -137,35 +144,36 @@ def storeInVTKpv(u, filename, writePointData = False):
         if writePointData:
             grid.point_data["u"] = u.getVertexValues()
         grid.cell_data["u"] = u.getValues()
-    grid.save(filename, binary=False) 
+    grid.save(filename, binary=False)
+
 
 # functions expects sigmas to be triangleData if its not a parameter object
 # until now this function can only write point_data, because compute_derivative()
 # returns point_data
 def storeFluxInVTK(field, u, sigmas, filename):
-    n = numberOfVertices()    
-    m = numberOfTriangles()    
-    v = field.grad(u)   
+    n = m.numberOfVertices()
+    numTriangles = m.numberOfTriangles()
+    v = field.grad(u)
     if isinstance(sigmas, Parameter):
         sigmaValues = sigmas.getValues()
     else:
         sigmaValues = sigmas
-    if len(sigmaValues) != m:
+    if len(sigmaValues) != numTriangles:
         print("Error: parameter is not defined for all triangles!")
-        exit()
-    pointSigmas = np.zeros((n,2,2))
-    for i, triangle in enumerate(mesh()['pt']):
+        sys.exit()
+    pointSigmas = np.zeros((n, 2, 2))
+    for i, triangle in enumerate(m.mesh()["pt"]):
         pointSigmas[triangle[0]] = sigmaValues[i]
         pointSigmas[triangle[1]] = sigmaValues[i]
         pointSigmas[triangle[2]] = sigmaValues[i]
-    flux = np.zeros((len(v),3))
+    flux = np.zeros((len(v), 3))
     for i in range(len(v)):
         flux[i][0:2] = pointSigmas[i] @ v[i][0:2]
-    points = np.hstack([mesh()['xp'], np.zeros((n,1))]) # add z coordinate
-    cells = (np.hstack([(3*np.ones((m,1))), mesh()['pt']])).ravel().astype(np.int64)
-    celltypes = np.empty(m, np.uint8)
-    celltypes[:] = vtk.VTK_TRIANGLE    
-    grid = pv.UnstructuredGrid(cells, celltypes, points)        
+    points = np.hstack([m.mesh()["xp"], np.zeros((n, 1))])  # add z coordinate
+    cells = (np.hstack([(3 * np.ones((m, 1))), m.mesh()["pt"]])).ravel().astype(np.int64)
+    celltypes = np.empty(numTriangles, np.uint8)
+    celltypes[:] = vtk.VTK_TRIANGLE
+    grid = pv.UnstructuredGrid(cells, celltypes, points)
     grid.point_data["u"] = u
     grid.point_data["flux"] = flux
-    grid.save(filename)      
+    grid.save(filename)
--- parameter.py
+++ parameter.py
@@ -1,3 +1,4 @@
+"""Module providing functions needed handle parameters in FEM formulations."""
 import numpy as np
 import sys
 import mesh as m
@@ -5,6 +6,7 @@ import mesh as m
 parameterList = []
 
 # TODO: make parameter able to handle regions of different dimensions
+
 
 class Parameter:
     def __init__(self, rows=1):
@@ -19,13 +21,12 @@ class Parameter:
             if self.parameterDimension == 0:
                 self.parameterDimension = m.regionDimension(setting[0])
             elif m.regionDimension(setting[0]) != self.parameterDimension:
-                print(f'Error: cannot mix regions with different dimensions in a parameter!')
+                print("Error: cannot mix regions with different dimensions in a parameter!")
                 sys.exit()
 
     def set(self, regions, value):
-        if ((not isinstance(value, list) and self.rows != 1) or 
-            (isinstance(value, list) and self.rows != len(value))):
-            print(f'Error: values for this parameter need to have {self.rows:d} rows!')
+        if (not isinstance(value, list) and self.rows != 1) or (isinstance(value, list) and self.rows != len(value)):
+            print(f"Error: values for this parameter need to have {self.rows:d} rows!")
             sys.exit()
         if not isinstance(regions, list):
             regions = [regions]
@@ -36,48 +37,93 @@ class Parameter:
         self.checkDimensions()
 
     def prepareValues(self, ids):
-        self.preparedValues[str(ids)] = np.empty((0,self.rows))    
-        preparedValues2 = dict()    
-        preparedValues2[str(ids)] = []        
+        self.preparedValues[str(ids)] = np.empty((0, self.rows))
+        preparedValues2 = dict()
+        preparedValues2[str(ids)] = []
         for setting in self.settings:
             if not setting[0] in ids:
                 continue
-            if self.rows == 1: # there is probably also a way to do it without this if-else
-                self.preparedValues[str(ids)] = np.append(self.preparedValues[str(ids)], 
-                    np.repeat(setting[1], np.count_nonzero(m.getMesh()['physical'][0] == setting[0])))
-                self.preparedValues[str(ids)] = np.append(self.preparedValues[str(ids)], 
-                    np.repeat(setting[1], np.count_nonzero(m.getMesh()['physical'][1] == setting[0])))
-                self.preparedValues[str(ids)] = np.append(self.preparedValues[str(ids)], 
-                    np.repeat(setting[1], np.count_nonzero(m.getMesh()['physical'][2] == setting[0])))
+            if self.rows == 1:  # there is probably also a way to do it without this if-else
+                self.preparedValues[str(ids)] = np.append(
+                    self.preparedValues[str(ids)],
+                    np.repeat(
+                        setting[1],
+                        np.count_nonzero(m.getMesh()["physical"][0] == setting[0]),
+                    ),
+                )
+                self.preparedValues[str(ids)] = np.append(
+                    self.preparedValues[str(ids)],
+                    np.repeat(
+                        setting[1],
+                        np.count_nonzero(m.getMesh()["physical"][1] == setting[0]),
+                    ),
+                )
+                self.preparedValues[str(ids)] = np.append(
+                    self.preparedValues[str(ids)],
+                    np.repeat(
+                        setting[1],
+                        np.count_nonzero(m.getMesh()["physical"][2] == setting[0]),
+                    ),
+                )
             else:
-                self.preparedValues[str(ids)] = np.row_stack((self.preparedValues[str(ids)], 
-                    np.tile(setting[1], (np.count_nonzero(m.getMesh()['physical'][0] == setting[0]),1))))
-                self.preparedValues[str(ids)] = np.row_stack((self.preparedValues[str(ids)], 
-                    np.tile(setting[1], (np.count_nonzero(m.getMesh()['physical'][1] == setting[0]),1))))
-                self.preparedValues[str(ids)] = np.row_stack((self.preparedValues[str(ids)], 
-                    np.tile(setting[1], (np.count_nonzero(m.getMesh()['physical'][2] == setting[0]),1))))
+                self.preparedValues[str(ids)] = np.row_stack(
+                    (
+                        self.preparedValues[str(ids)],
+                        np.tile(
+                            setting[1],
+                            (
+                                np.count_nonzero(m.getMesh()["physical"][0] == setting[0]),
+                                1,
+                            ),
+                        ),
+                    )
+                )
+                self.preparedValues[str(ids)] = np.row_stack(
+                    (
+                        self.preparedValues[str(ids)],
+                        np.tile(
+                            setting[1],
+                            (
+                                np.count_nonzero(m.getMesh()["physical"][1] == setting[0]),
+                                1,
+                            ),
+                        ),
+                    )
+                )
+                self.preparedValues[str(ids)] = np.row_stack(
+                    (
+                        self.preparedValues[str(ids)],
+                        np.tile(
+                            setting[1],
+                            (
+                                np.count_nonzero(m.getMesh()["physical"][2] == setting[0]),
+                                1,
+                            ),
+                        ),
+                    )
+                )
 
     def getValues(self, region=[]):
         ids = []
         for x in self.settings:
             ids.append(x[0])
 
-        if region == []: # use all regions if no region is specified
+        if region == []:  # use all regions if no region is specified
             regionIds = ids
         else:
             # check if all ids exist in parameter
-            for id in region.ids:
-                if not id in ids:
-                    print(f'Error: Region id {id:d} not present in parameter!')
+            for regionId in region.ids:
+                if regionId not in ids:
+                    print(f"Error: Region id {id:d} not present in parameter!")
                     sys.exit()
             regionIds = region.ids
-            
+
         # use calculated values if possible
         if not str(regionIds) in self.preparedValues:
             self.prepareValues(regionIds)
 
         return self.preparedValues[str(regionIds)]
-    
+
     # this function is quite inefficient, but its only for debug purpose anyway
     # Problem: vertexValues at the border of a region get overwritten by neighbouring region
     # but its only a problem for visualization, since this function is not used in calculation
@@ -91,7 +137,7 @@ class Parameter:
         else:
             vertexValues = np.zeros((m.numberOfVertices()))
         for n in range(m.numberOfTriangles()):
-            vertexValues[m.getMesh()['pt'][n][0]] = triangleValues[n]
-            vertexValues[m.getMesh()['pt'][n][1]] = triangleValues[n]
-            vertexValues[m.getMesh()['pt'][n][2]] = triangleValues[n]
-        return vertexValues     
+            vertexValues[m.getMesh()["pt"][n][0]] = triangleValues[n]
+            vertexValues[m.getMesh()["pt"][n][1]] = triangleValues[n]
+            vertexValues[m.getMesh()["pt"][n][2]] = triangleValues[n]
+        return vertexValues
